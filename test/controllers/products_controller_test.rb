9570eab916364744a00776a0b165154ff454687d
Marissa Abao <mabao@rrc.ca>
2025-07-03 10:09:15 -0500

    Modified Replaced products_index_url to products_url and added a test for show action too

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,8 +1,20 @@
+# Name: Marissa Abao
+# Submission Date: July 4, 2025
+# Course: WEBD-3011 Agile Full Stack Web Development
+# Description: U5 - Rails, Faker, and Git Challenge
+# This test checks that the products index page loads successfully.
+
 require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get products_index_url
+    get products_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    product = Product.first || Product.create(title: "Test", price: 1.0, stock_quantity: 1)
+    get product_url(product)
     assert_response :success
   end
 end
