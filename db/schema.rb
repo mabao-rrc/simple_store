851adda22c9a529d15300a0c028779778868cc4a
Marissa Abao <mabao@rrc.ca>
2025-07-03 17:13:25 -0500

    Generate Category model with name property

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_03_020006) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_03_220657) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
